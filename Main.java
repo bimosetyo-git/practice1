cf6e1c2db62f45f3c1ca5be806beee5f2454475b
bimosetyo-git <57379273+bimosetyo-git@users.noreply.github.com>
2019-11-05 09:57:44 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Version 1.0");
+		System.out.println("Version 2.0");
 		System.out.println("");
 		for (int i=1;i<=10;i++) {
 			for(int j=10;j>=i;j--) {
